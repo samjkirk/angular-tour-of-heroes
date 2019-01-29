af0e66438f281e7b605ef4866453188d858a44c2
sam kirk <samueljkirk10@gmail.com>
2019-01-29 14:15:45 +0000

    added services

---
+++ src/app/hero.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+import { Observable, of } from 'rxjs';
+import { MessagesService } from './messages.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class HeroService {
+
+  getHeroes(): Observable<Hero[]> {
+    this.messageService.add('HeroService: fetched heroes');
+    return of(HEROES);
+  }
+
+  constructor(private messageService: MessagesService) { }
+}
